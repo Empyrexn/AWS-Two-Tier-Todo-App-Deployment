9401fc774486b60baa2e1c108036f4a2e8cc81d0
Jose Lopez <142949114+Empyrexn@users.noreply.github.com>
2025-05-23 14:59:58 -0700

    Create vpc.tf

---
+++ VPC/vpc.tf
@@ -0,0 +1,108 @@
+# Creating new VPC 
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr
+
+    tags = {
+      Name = "two-tier-vpc"
+    }
+}
+
+data "aws_availability_zones" "available" {}
+
+# Creating public subnets 
+resource "aws_subnet" "public_a" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-a"
+    }
+}
+
+resource "aws_subnet" "public_b" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = data.aws_availability_zones.available.names[1]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-b"
+    }
+}
+
+# Creating private subnets
+resource "aws_subnet" "private_a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "private-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "private-subnet-b"
+  }
+}
+
+# Creating internet gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "two-tier-igw"
+    }
+}
+
+# Creating public route table
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id =  aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "public-route-table"
+    }
+}
+
+# Creating private route table 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Private subnets being routed to private route table
+resource "aws_route_table_association" "private_a_assoc" {
+    subnet_id = aws_subnet.private_a.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_b_assoc" {
+    subnet_id = aws_subnet.private_b.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+# Public subnets being routed to public route table
+resource "aws_route_table_association" "public_a_assoc" {
+    subnet_id = aws_subnet.public_a.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_b_assoc" {
+    subnet_id = aws_subnet.public_b.id
+    route_table_id = aws_route_table.public_rt.id
+}
