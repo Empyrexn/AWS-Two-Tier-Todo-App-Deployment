1a3a3c9ea04e1fe7b619cb7c4c0341cc84881670
Jose Lopez <142949114+Empyrexn@users.noreply.github.com>
2025-05-27 13:29:20 -0700

    Update ec2.tf

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web1" {
-  ami           = "ami-07706bb32254a7fe5"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_a.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  key_name = "bastion-host-key"
+  ami                         = "ami-07706bb32254a7fe5"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_a.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = "bastion-host-key"
   associate_public_ip_address = true
 
   user_data = templatefile("${path.module}/user_data.tpl", {
@@ -20,11 +20,11 @@ resource "aws_instance" "web1" {
 }
 
 resource "aws_instance" "web2" {
-  ami           = "ami-07706bb32254a7fe5"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_a.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  key_name = "bastion-host-key"
+  ami                         = "ami-07706bb32254a7fe5"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_a.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = "bastion-host-key"
   associate_public_ip_address = true
 
   user_data = templatefile("${path.module}/user_data.tpl", {
@@ -42,11 +42,11 @@ resource "aws_instance" "web2" {
 
 # Bastion host ec2 in order to initialize mySQL
 resource "aws_instance" "bastion" {
-  ami           = "ami-07706bb32254a7fe5"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_a.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  key_name = "bastion-host-key"
+  ami                         = "ami-07706bb32254a7fe5"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_a.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = "bastion-host-key"
   associate_public_ip_address = true
 
   user_data = templatefile("${path.module}/bastion_host_data.tpl", {
@@ -63,15 +63,15 @@ resource "aws_instance" "bastion" {
 
 resource "aws_eip" "web1_ip" {
   instance = aws_instance.web1.id
-  domain = "vpc"
+  domain   = "vpc"
 }
 
 resource "aws_eip" "web2_ip" {
   instance = aws_instance.web2.id
-  domain = "vpc"
+  domain   = "vpc"
 }
 
 resource "aws_eip" "bastion_ip" {
-    instance = aws_instance.bastion.id
-    domain = "vpc"
+  instance = aws_instance.bastion.id
+  domain   = "vpc"
 }
