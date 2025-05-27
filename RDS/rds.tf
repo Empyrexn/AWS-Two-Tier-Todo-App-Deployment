dcb4ea13ca7bce5dc41fefc9403ec911eb5a3f5d
Jose Lopez <142949114+Empyrexn@users.noreply.github.com>
2025-05-27 13:26:52 -0700

    Update rds.tf

--- RDS/rds.tf
+++ RDS/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "main" {
-  name = "two-tier-db-subnet-group"
+  name       = "two-tier-db-subnet-group"
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 
   tags = {
@@ -9,17 +9,17 @@ resource "aws_db_subnet_group" "main" {
 
 # Database creation
 resource "aws_db_instance" "main" {
-  identifier = "two-tier-db"
-  engine = "mysql"
-  instance_class = "db.t3.micro"
-  allocated_storage = 20
-  username = "admin"
-  password = var.db_password
-  db_name = "tododb"
-  publicly_accessible = false
-  skip_final_snapshot = true
+  identifier             = "two-tier-db"
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "admin"
+  password               = var.db_password
+  db_name                = "tododb"
+  publicly_accessible    = false
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.main.name
+  db_subnet_group_name   = aws_db_subnet_group.main.name
 
   tags = {
     Name = "two-tier-db"
